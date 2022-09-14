34572844e91cdc44f0e7380d75d0ecb0d20cef5b
JamyPut <jp2001dj@gmail.com>
2022-09-14 23:50:27 +0200

    added onclick event with function and arrow function

--- src/App.js
+++ src/App.js
@@ -3,65 +3,75 @@ import React from 'react';
 import citroen from "./assets/citroenen.jpeg";
 import ijsblokjes from "./assets/ijsblokjes.jpg";
 import limoen from "./assets/limoenen.png"
-import { ReactComponent as ShoppingCart } from './assets/winkelmandje.svg';
+import {ReactComponent as ShoppingCart} from './assets/winkelmandje.svg';
 
 import './App.css';
 
 function App() {
-  return (
-    <>
-    <nav>
-        <ul>
-            <li><a href="/">Shop</a></li>
-            <li><a href="/">Ons verhaal</a></li>
-            <li><a href="/">Blog</a></li>
-        </ul>
-        <ShoppingCart className={"shopping-cart-icon"}/>
-    </nav>
-    <header>
-        <h1>Fruit perfection</h1>
-        <button type={"button"} >
-            Shop nu!
-        </button>
-    </header>
-    <main>
-        <article className={"product"}>
-            <img
-                src={citroen}
-                alt={"citroen"}
-            />
-            <h2 className={"product-name"}>Citroen</h2>
-            <p className={"product-description"}>
-                Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten.
-                Van citroen kun je het vruchtvlees, het sap en de schil gebruiken. Het
-                sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel.
-            </p>
-        </article>
-        <article className={"product"}>
-            <img
-                src={limoen}
-                alt={"citroen"}
-            />
-            <h2 className={"product-name"}>Limoen</h2>
-            <p className={"product-description"}>
-                Limoen is familie van de citroen en de sinaasappel en behoort tot de citrusvruchten (Wijnruitfamilie).
-                Limoenen zijn rond en kleiner dan citroenen. De schil is dun, vrij glad en groen.
-            </p>
-        </article>
-        <article className={"product"}>
-            <img
-                src={ijsblokjes}
-                alt={"citroen"}
-            />
-            <h2 className={"product-name"}>Ijsblokjes</h2>
-            <p className={"product-description"}>
-                Een ijsblokje of ijsklontje is bevroren water in de vorm van een klein blokje.
-                Het wordt gemaakt in een diepvriezer door water in een plastic vorm te laten bevriezen.
-            </p>
-        </article>
-    </main>
-    </>
-  );
+    function logClick() {
+        console.log("You Clicked!");
+    }
+
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li><a href="/">Shop</a></li>
+                    <li><a href="/">Ons verhaal</a></li>
+                    <li><a href="/">Blog</a></li>
+                </ul>
+                <ShoppingCart className={"shopping-cart-icon"}/>
+            </nav>
+            <header>
+                <h1>Fruit perfection</h1>
+                {/*on clickevent met arrow functie*/}
+                {/*<button type={"button"} onClick={() => console.log("ik weet heel zeker dat je hebt geklikt")}>*/}
+                {/*    Shop nu!*/}
+                {/*</button>*/}
+                <button type={"button"} onClick={logClick}>
+                    Shop nu!
+                </button>
+
+            </header>
+            <main>
+                <article className={"product"}>
+                    <img
+                        src={citroen}
+                        alt={"citroen"}
+                    />
+                    <h2 className={"product-name"}>Citroen</h2>
+                    <p className={"product-description"}>
+                        Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten.
+                        Van citroen kun je het vruchtvlees, het sap en de schil gebruiken. Het
+                        sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel.
+                    </p>
+                </article>
+                <article className={"product"}>
+                    <img
+                        src={limoen}
+                        alt={"citroen"}
+                    />
+                    <h2 className={"product-name"}>Limoen</h2>
+                    <p className={"product-description"}>
+                        Limoen is familie van de citroen en de sinaasappel en behoort tot de citrusvruchten
+                        (Wijnruitfamilie).
+                        Limoenen zijn rond en kleiner dan citroenen. De schil is dun, vrij glad en groen.
+                    </p>
+                </article>
+                <article className={"product"}>
+                    <img
+                        src={ijsblokjes}
+                        alt={"citroen"}
+                    />
+                    <h2 className={"product-name"}>Ijsblokjes</h2>
+                    <p className={"product-description"}>
+                        Een ijsblokje of ijsklontje is bevroren water in de vorm van een klein blokje.
+                        Het wordt gemaakt in een diepvriezer door water in een plastic vorm te laten bevriezen.
+                    </p>
+                </article>
+            </main>
+        </>
+    );
 }
 
 export default App;
